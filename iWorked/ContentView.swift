e2121c28b387b4dc957e031e207120255f83b25b
Antoine <bababouch07@gmail.com>
2020-08-31 15:29:38 +0200

    Initial commit

---
+++ iWorked/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  iWorked
+//
+//  Created by Antoine on 31/08/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iWorked/iWorkedApp.swift
@@ -0,0 +1,17 @@
+//
+//  iWorkedApp.swift
+//  iWorked
+//
+//  Created by Antoine on 31/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct iWorkedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
